9d53ab2f460951c8a8472f2e385ced2541b5777e
Adam <adam.jarrett.clare@gmail.com>
2021-01-10 19:30:40 -0700

    Add option to change color of triangle using vertex to fragmenet shader pipeline

--- sb/animated_triangle/animated_triangle.cpp
+++ sb/animated_triangle/animated_triangle.cpp
@@ -9,6 +9,8 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 static const char* vs_src = {
   "#version 400 core                                                 \n"
   "layout (location = 0) in vec4 offset;                             \n"
+  "layout (location = 1) in vec4 color;                              \n"
+  "out vec4 vs_color;                                                \n"
   "void main(void)                                                   \n"
   "{                                                                 \n"
   "    const vec4 vertices[] = vec4[](vec4( 0.25, -0.25, 0.5, 1.0),  \n"
@@ -16,16 +18,18 @@ static const char* vs_src = {
   "                                   vec4( 0.25,  0.25, 0.5, 1.0)); \n"
   "                                                                  \n"
   "    gl_Position = vertices[gl_VertexID] + offset;                 \n"
+  "    vs_color = color;                                             \n"
   "}                                             \n"
 };
 
 /* Fragment shader source */
 static const char* fs_src = {
     "#version 400 core                             \n"
+    "in vec4 vs_color;                             \n"
     "out vec4 color;                               \n"
     "void main(void)                               \n"
     "{                                             \n"
-    "    color = vec4(0.0, 0.8, 1.0, 1.0);         \n"
+    "    color = vs_color;                         \n"
     "}                                             \n"
 };
 
@@ -40,7 +44,7 @@ int main() {
   Display display("Animated Triangle", 800, 600);
   std::cout << "Display a single triangle" << std::endl;
   display.SetClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-  // glfwSetKeyCallback(display.GetWindow(), key_callback);
+  glfwSetKeyCallback(display.GetWindow(), key_callback);
 
   /* Load shaders and create a shader program */
   unsigned int program = LoadShaders(vs_src, fs_src);
@@ -57,22 +61,30 @@ int main() {
       std::cos(glfwGetTime()) * 0.6f,
       0.0f, 0.0f};
     glVertexAttrib4fv(0, attrib);
+
     if (mode == 1) {
-      glDrawArrays(GL_TRIANGLES, 0, 3);
+      float color[4] = {0.0f, 0.8f, 1.0f, 1.0f};
+      glVertexAttrib4fv(1, color);
     }
+    if (mode == 2) {
+      float color[4] = {
+        0.0f,
+        std::sin(glfwGetTime()) * 0.5f + 0.5f,
+        std::cos(glfwGetTime()) * 0.5f + 0.5f,
+        1.0f};
+      glVertexAttrib4fv(1, color);
+    }
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     
     display.Update();
   }
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-  // if (key == GLFW_KEY_1 && action == GLFW_PRESS) {
-  //   mode = 1;
-  // }
-  // if (key == GLFW_KEY_2 && action == GLFW_PRESS) {
-  //   mode = 2;
-  // }
-  // if (key == GLFW_KEY_3 && action == GLFW_PRESS) {
-  //   mode = 3;
-  // }
+  if (key == GLFW_KEY_1 && action == GLFW_PRESS) {
+    mode = 1;
+  }
+  if (key == GLFW_KEY_2 && action == GLFW_PRESS) {
+    mode = 2;
+  }
 }
